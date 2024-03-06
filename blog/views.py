b9ce3b55763c3c810036f03eca88c9416773532b
jishnuraj rs <jishnuraj017@gmail.com>
2024-03-06 18:22:43 +0530

    initial commit

---
+++ blog/views.py
@@ -0,0 +1,80 @@
+from django.shortcuts import render, redirect,get_object_or_404
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from .models import Post
+from . forms import PostForm
+from django.contrib import messages
+
+
+
+
+# Create your views here.
+
+
+def home(request):
+
+    posts = Post.objects.all()
+
+    context = {
+        'posts': posts,
+        'title': 'Zen of Python'
+    }
+    return render(request, 'blog/home.html', context)
+
+def about(request):
+    return render(request, 'blog/about.html')
+
+@login_required
+def create_post(request):
+    if request.method == 'GET' :
+        context = {
+            'form': PostForm()
+        }
+        return render(request, 'blog/post_form.html', context)
+    elif request.method == 'POST':
+        form  =PostForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.author = request.user
+            form.save()
+            messages.success(request, 'The post has been created successfully.')
+            return redirect('posts')
+        else:
+            messages.error(request, 'Please correct the following errors:')
+            return render(request, 'blog/poat_form.html', {'form':form})
+
+@login_required
+def edit_post(request, id):
+    queryset = Post.objects.filter(author=request.user)
+    post = get_object_or_404(queryset, pk=id)
+
+    if request.method == 'GET':
+        context = {'form': PostForm(instance=post), 'id': id}
+        return render(request,'blog/post_form.html',context)
+    
+    elif request.method == 'POST':
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'The post has been updated successfully.')
+            return redirect('posts')
+        else:
+            messages.error(request, 'Please correct the following errors:')
+            return render(request,'blog/post_form.html',{'form':form})
+        
+
+
+@login_required
+def delete_post(request, id):
+    queryset = Post.objects.filter(author=request.user)
+
+    post = get_object_or_404(queryset, pk=id)
+    context = {'post':post}
+
+    if request.method == 'GET':
+        return render(request, 'blog/post_confirm_delete.html', context)
+    
+    elif request.method == 'POST':
+        post.delete()
+        messages.success(request,  'The post has been deleted successfully.')
+        return redirect('posts')
